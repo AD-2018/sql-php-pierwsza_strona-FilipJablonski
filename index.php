53cfbcdbb9bdfe1e1dc370ed2b0ebdfdc2b51295
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-10-30 11:52:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-  echo "Filip Jablonski";
- ?>
+
+$conn = new mysqli("mysql-jablonski-filip.alwaysdata.net","217189","4CM@rD.M6Bvc3Bx","jablonski-filip_pbd");
+$sql = "SELECT * FROM pracownicy";
+$wynik = mysqli_query($conn, $sql);
+
+
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik)){
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+
+
+?>
