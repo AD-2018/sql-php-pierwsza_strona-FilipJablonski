2eaddd7bd0f716adc7ebcd49ac9e37f3203f9e2d
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-11-05 09:18:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -25,44 +25,6 @@ echo("</tr>");
 }
 echo('</table>');
     
-echo("<a>Zadanie 2</a>")
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['dzial']."</td><td>".$row['zarobki']."</td>);
-    echo("<tr>");
-echo("</tr>");
-}
-echo('</table>');
-        
-echo("<a>Zadanie 3</a>")
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-    echo("<tr>");
-echo("</tr>");
-}
-echo('</table>');
-        
-echo("<a>Zadanie 4</a>")
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-    echo("<tr>");
-echo("</tr>");
-}
-echo('</table>');
-        
-?>
-
 
 </body>
 </html>
