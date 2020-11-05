844cb586c7bd68fd1cc0293e2cc7ec2d2bea8bdc
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-11-05 08:20:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="mystyle.css">
+</head>
+<body>
 <?php
 
 $conn = new mysqli("mysql-jablonski-filip.alwaysdata.net","217189","4CM@rD.M6Bvc3Bx","jablonski-filip_pbd");
-$sql = "SELECT * FROM pracownicy";
-$wynik = mysqli_query($conn, $sql);
 
+$sql ="select * from pracownicy";
+$result = mysqli_query($conn, $sql);
 
-    echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
-
-    while($wiersz=mysqli_fetch_assoc($wynik)){
-        echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
-
-
+echo('<table border="1" class="tabela"');
+echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
+while($row=mysqli_fetch_assoc($result)){
+echo("<tr>");
+    echo("<tr>");
+    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
+    echo("<tr>");
+echo("</tr>");
+}
+echo('</table>');
 ?>
+
+
+</body>
+</html>
