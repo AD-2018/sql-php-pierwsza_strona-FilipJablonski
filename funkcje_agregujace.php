f592681127929aead7b9b2db7d081025102a6320
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-11-06 10:10:26 +0100

    Update funkcje_agregujace.php

--- funkcje_agregujace.php
+++ funkcje_agregujace.php
@@ -17,22 +17,15 @@
 <?php
 
 require_once("connect.php");
-
-echo("<h3>tabelka</h3>"); 
-$result = mysqli_query($conn, $sql);  
-echo('<table border="1" class="tabela"'); 
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
-while($row=mysqli_fetch_assoc($result)){ 
-  echo("<tr>");     
-  echo("<tr>");     
-  echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");     
-  echo("<tr>"); 
-  echo("</tr>"); } 
-echo('</table>'); 
     
     $sql ="select avg(zarobki),nazwa_dzial from pracownicy,organizacja where id_org=dzial and imie not like '%a' group by dzial having avg(zarobki)<35"; 
 echo("<h3>zadanie1</h3>"); 
-$result = mysqli_query($conn, $sql);  
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
 echo('<table border="1" class="tabela"'); 
 echo ("<tr><th>avg(zarobki)</th><th>nazwa_dzial</th></tr>"); 
 while($row=mysqli_fetch_assoc($result)){ 
@@ -45,7 +38,12 @@ echo('</table>');
   
       $sql ="select avg(zarobki),nazwa_dzial from pracownicy,organizacja where id_org=dzial group by dzial having avg(zarobki)<40"; 
 echo("<h3>zadanie2</h3>"); 
-$result = mysqli_query($conn, $sql);  
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
 echo('<table border="1" class="tabela"'); 
 echo ("<tr><th>avg(zarobki)</th><th>nazwa_dzial</th></tr>"); 
 while($row=mysqli_fetch_assoc($result)){ 
@@ -58,7 +56,12 @@ echo('</table>');
     
       $sql ="select imie, zarobki from pracownicy,organizacja where id_org=dzial and imie not like '%a' group by imie having zarobki<40"; 
 echo("<h3>zadanie3</h3>"); 
-$result = mysqli_query($conn, $sql);  
+$result = mysqli_query($conn, $sql); 
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
 echo('<table border="1" class="tabela"'); 
 echo ("<tr><th>imie</th><th>zarobki</th></tr>"); 
 while($row=mysqli_fetch_assoc($result)){ 
@@ -71,7 +74,12 @@ echo('</table>');
     
       $sql ="select * from pracownicy,organizacja where id_org=dzial group by imie having dzial=2 or dzial=3"; 
 echo("<h3>zadanie4</h3>"); 
-$result = mysqli_query($conn, $sql);  
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
 echo('<table border="1" class="tabela"'); 
 echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
 while($row=mysqli_fetch_assoc($result)){ 
