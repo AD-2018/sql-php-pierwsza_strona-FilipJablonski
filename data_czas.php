3a8260211a02b32ee383d353f6f8e9de0a992c76
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-11-06 22:06:12 +0100

    Update data_czas.php

--- data_czas.php
+++ data_czas.php
@@ -1,1 +1,25 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <h1>Filip Jabłoński</h1>
+<nav>
+    <br>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-FilipJablonski">Github</a>
+    
+    <div class="nav">
+         <a href="index.php">Strona Główna</a>  
+         <a href="pracownicy.php">Pracownicy</a>
+         <a href="pracownicy_organizacja.php">Pracow. i Org.</a>   
+         <a href="funkcje_agregujace.php">Funkcje Agregujace</a>  
+         <a href="data_czas.php">Data i Czas</a>
+    </div>
+    <br>
+</nav>  
+<?php
+?>
+</body>
+</html>
