9d4d9027e3813ec74a9c681c86ea5292bdba2b6d
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-11-06 10:01:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,16 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-FilipJablonski/">github</a>
+    
     <h1>Filip Jabłoński</h1>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-FilipJablonski">Github</a>
+    
+    <div class="nav">
+         <a href="index.php">Strona Główna</a>   
+         <a href="pracownicy_organizacja.php">Pracow. i Org.</a>   
+         <a href="funkcje_agregujace.php">Funkcje Agregujace</a>   
+    </div>
+    
 <?php
 
 $conn = new mysqli("mysql-jablonski-filip.alwaysdata.net","217189","4CM@rD.M6Bvc3Bx","jablonski-filip_pbd");
