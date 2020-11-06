306c7fc4e777daa900fdfa8e77f70020f5b921f5
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-11-06 12:03:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,11 @@
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-FilipJablonski">Github</a>
     
     <div class="nav">
+        <nav>
          <a href="index.php">Strona Główna</a>   
          <a href="pracownicy_organizacja.php">Pracow. i Org.</a>   
          <a href="funkcje_agregujace.php">Funkcje Agregujace</a>   
+        </nav>
     </div>
 </body>
 </html>
