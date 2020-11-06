36cc91923fe40eb230c246204d9e08252f1de9a6
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-11-06 09:51:11 +0100

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "mysql-jablonski-filip.alwaysdata.net";
+$username = "217189";
+$password = "4CM@rD.M6Bvc3Bx";
+$dbname = "jablonski-filip_pbd";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+>
