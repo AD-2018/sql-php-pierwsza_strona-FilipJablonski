d0240e83c1978a158f14d4b13d45a6aee6bf664c
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-11-06 12:42:27 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -76,7 +76,23 @@ while($row=mysqli_fetch_assoc($result)){
 echo('</table>');   
     
 echo("<h2>Pracownicy i Organizacja</h2>")
-
+      $sql ="select imie, zarobki from pracownicy,organizacja where id_org=dzial group by imie"; 
+echo("<h3>Zadanie 1</h3>"); 
+$result = mysqli_query($conn, $sql); 
+if ( $result) {
+        echo "<br>";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>imie</th><th>nazwa dzialu</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");     
+  echo("<tr>");     
+  echo("<td>".$row['imie']."</td><td>".$row['nazwa_dzial']."</td>");     
+  echo("<tr>"); 
+  echo("</tr>"); } 
+echo('</table>'); 
 ?>
 </body>
 </html>
