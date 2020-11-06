862a0607626b6b8fe152e14bbf27d68346ae9cbb
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-11-06 10:13:46 +0100

    Create pracownicy_organizacja.php

---
+++ pracownicy_organizacja.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <h1>Filip Jabłoński</h1>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-FilipJablonski">Github</a>
+    
+    <div class="nav">
+         <a href="index.php">Strona Główna</a>   
+         <a href="pracownicy_organizacja.php">Pracow. i Org.</a>   
+         <a href="funkcje_agregujace.php">Funkcje Agregujace</a>   
+    </div>
+    
+<?php
+require_once("connect.php");
+echo("<h3>tabelka</h3>"); 
+$result = mysqli_query($conn, $sql);  
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }    
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");     
+  echo("<tr>");     
+  echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");     
+  echo("<tr>"); 
+  echo("</tr>"); } 
+echo('</table>'); 
+?>
+</body>
+</html>
