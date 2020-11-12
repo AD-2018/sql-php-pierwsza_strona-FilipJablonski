8daf726ad518d639ca9e4fde0e53801ea7e82b4b
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-11-12 08:18:47 +0100

    Update data_czas.php

--- data_czas.php
+++ data_czas.php
@@ -38,8 +38,24 @@ while($row=mysqli_fetch_assoc($result)){
   echo("<td>".$row['imie']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['wiek']."</td>");     
   echo("</tr>"); } 
 echo('</table>'); 
+              
+    $sql ="select *,YEAR(curdate())-YEAR(data_urodzenia) AS wiek from pracownicy,organizacja where id_org=dzial and dzial=1"; 
+echo("<h3>Zadanie 2</h3>"); 
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<br>";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>Imie</th><th>Dzial</th><th>Wiek</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");         
+  echo("<td>".$row['imie']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['wiek']."</td>");     
+  echo("</tr>"); } 
+echo('</table>'); 
           
-    $sql ="select *,YEAR(curdate())-YEAR(data_urodzenia) AS wiek,Sum(wiek) from pracownicy,organizacja where id_org=dzial and dzial=1"; 
+    $sql ="select *,SUM(YEAR(CURDATE()) - YEAR(data_urodzenia)) as Suma from pracownicy,organizacja where id_org=dzial"; 
 echo("<h3>Zadanie 3</h3>"); 
 $result = mysqli_query($conn, $sql);
 if ( $result) {
@@ -51,7 +67,7 @@ echo('<table border="1" class="tabela"');
 echo ("<tr><th>Suma Lat</th></tr>"); 
 while($row=mysqli_fetch_assoc($result)){ 
   echo("<tr>");         
-  echo("<td>".$row['Sum(wiek)']."</td>");     
+  echo("<td>".$row['Suma']."</td>");     
   echo("</tr>"); } 
 echo('</table>'); 
      
