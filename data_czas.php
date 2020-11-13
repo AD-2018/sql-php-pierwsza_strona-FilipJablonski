516c640b13a5854af83ad497284e6a9afadbe02a
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-11-13 10:17:27 +0100

    Update data_czas.php

--- data_czas.php
+++ data_czas.php
@@ -283,6 +283,23 @@ while($row=mysqli_fetch_assoc($result)){
   echo("<td>".$row['data']."</td>");     
   echo("</tr>"); } 
 echo('</table>'); 
+                                              
+    $sql ="select *, DATE_FORMAT(data_urodzenia,'%Y-%M-%W') as data from pracownicy"; 
+echo("<h3>Zadanie 5</h3>"); 
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>".$sql."</li>";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>Imie</th><th>Data Urodzenia</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");         
+  echo("<td>".$row['imie']."</td><td>".$row['data']."</td>");     
+  echo("</tr>"); } 
+echo('</table>'); 
+    
 ?>
 </body>
 </html>
