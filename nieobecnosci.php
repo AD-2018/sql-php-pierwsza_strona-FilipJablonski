3e25e5e913b10feb6fb8d333f31f56672cfa8d15
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-11-13 13:00:58 +0100

    Create nieobecnosci.php

---
+++ nieobecnosci.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <h1>Filip Jabłoński</h1>
+<nav>
+    <br>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-FilipJablonski">Github</a>
+    <br>
+    <br>
+    <div class="nav">
+         <a href="index.php">Strona Główna</a>  
+         <a href="pracownicy.php">Pracownicy</a>
+         <a href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>   
+         <a href="funkcje_agregujace.php">Funkcje Agregujace</a>  
+         <a href="data_czas.php">Data i Czas</a>
+         <a href="data_czas.php">Nieobecności Pracowników</a>
+    </div>
+    <br>
+</nav>  
+<?php
+?>
+</body>
+</html>
