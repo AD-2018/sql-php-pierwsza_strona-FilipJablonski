fa6e09b37b85b128f4b516c150805ce8fdba807d
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-11-20 10:30:40 +0100

    Update nieobecnosci.php

--- nieobecnosci.php
+++ nieobecnosci.php
@@ -19,9 +19,11 @@
          <a href="data_czas.php">Data i Czas</a>
          <a href="nieobecnosci.php">Nieobecności Pracowników</a>
          <a href="strona.php">Strona</a>
+         <a href="daneDoBazy.html">Dane Do Bazy</a>
+         <a href="insert.php">insert.php</a>
     </div>
     <br>
-</nav> 
+</nav>  
 <?php
 ?>
 </body>
