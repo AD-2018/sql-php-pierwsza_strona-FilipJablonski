8bee863fb77c85823d89f1a3cf8d1034466a5b9b
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-11-26 08:38:49 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,7 +2,7 @@
 require_once("connect.php");
 
 $sql = "INSERT INTO pracownicy (id,imie, dzial, zarobki,data_urodzenia) 
-       VALUES (null,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
+       VALUES (null,".$_POST['imie'].','.$_POST['dzial'].','.$_POST['zarobki'].','.$_POST['data_urodzenia'].')';
 
 
 if ($conn->query($sql) === TRUE) {
