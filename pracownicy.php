0d91d1a86c66edb83bbeec9f87f17db4b09bca3b
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-12-04 10:23:25 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -23,14 +23,6 @@
     </div>
     <br>
 </nav>
-<form action="pracownicy.php">
-  <label for="table">Pracownik:</label>
-  <select name=$row['imie'] id=$row['imie']>
-    <option value=$row['imie']>$row['imie']</option>
-  </select>
-  <br><br>
-  <input type="submit" value="Submit">
-</form>
 <?php
 
 require_once("connect.php");
