4ca2c40ade6a7bf887295f5a51cc3984824bbcef
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-12-05 23:24:39 +0100

    Create testowa_gra.php

---
+++ testowa_gra.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="style.css">
+  <title>Dane do Bazy</title>
+</head>
+<body>
+<nav>
+    <div class="nav">
+    <strong>
+    	 <li><a class="GitHub" href="https://github.com/AD-2018/sql-php-pierwsza_strona-FilipJablonski">Github</a></li>
+         <li><a href="index.php">Strona Główna</a></li>
+         <li><a href="pracownicy.php">Pracownicy</a></li>
+         <li><a href="pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>   
+         <li><a href="funkcje_agregujace.php">Funkcje Agregujace</a></li>
+         <li><a href="data_czas.php">Data i Czas</a></li>
+         <li><a href="nieobecnosci.php">Nieobecności Pracowników</a></li>
+         <li><a href="strona.php">Strona</a></li>
+         <li><a href="daneDoBazy.php">Dane Do Bazy</a></li>
+         <li><a href="testowa_gra.php">Testowa Gra</a></li>
+    </strong>
+    </div>
+	</br></br></br>
+</nav>
+<?php
+require_once("connect.php");
+$sql = "SELECT * FROM gra_nr_1";
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+         echo "<br>";
+     } else {
+       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+
+    echo("<h1>Tabelka</h1>");
+
+    echo("<table border='1'>");
+    echo("<th>Kolumna</th><th>Pole1</th><th>Pole2</th><th>Pole3</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo('<tr>');
+            echo('<td>'.$row[kolumna].'</td><td>'.$row['pole1'].'</td><td>'.$row['pole2'].'</td><td>'.$row['pole3'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+?>
