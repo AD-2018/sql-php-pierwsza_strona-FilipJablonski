6e9f2975547b11529a74fc7b8eca4527b7c4f866
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-12-07 19:09:46 +0100

    Update testowa_gra_restart.php

--- testowa_gra_restart.php
+++ testowa_gra_restart.php
@@ -7,7 +7,7 @@
 <?php
 require_once("connect.php");
 
-$sql = "UPDATE gra_nr_1 SET Kolumna1= ' ',Kolumna2= ' ',Kolumna3= ' ' WHERE X = 'Wiersz 1' or X = 'Wiersz 2' or X = 'Wiersz 3';"
+$sql = "UPDATE gra_nr_1 SET Kolumna1= ' ',Kolumna2= ' ',Kolumna3= ' ' WHERE X = 'Wiersz 1' or X = 'Wiersz 2' or X = 'Wiersz 3'";
 
 if ($conn->query($sql) === TRUE) {
   header('Location: https://jablonski-filip.herokuapp.com/testowa_gra.php');
