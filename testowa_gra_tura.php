41d1e78d1d7f22de19e13565c1e38a66f84c07dc
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-12-08 13:33:53 +0100

    Update testowa_gra_tura.php

--- testowa_gra_tura.php
+++ testowa_gra_tura.php
@@ -17,7 +17,7 @@ if ($conn->query($sql) === TRUE) {
 $sql = "UPDATE gra_nr_1 SET tura= if (".$_POST['Kolumna']."='X' , 'O','X') WHERE X = 'Wiersz 1'";
 
 if ($conn->query($sql) === TRUE) {
-  echo("<br>");
+  header('Location: https://jablonski-filip.herokuapp.com/testowa_gra.php');
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
