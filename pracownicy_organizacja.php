84b11539238c7797ff0a1887765ba40cef1a22d0
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-12-10 09:24:07 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -34,7 +34,7 @@ if ( $result) {
 echo('<select name="pracownik">');
 	while($row = mysqli_fetch_assoc($result) {
             echo ('<option value="'.$row['id_pracownicy'].'">');
-	    echo(".$row['id_pracownicy'].' '.$row['imie'].' '.$row['zarobki'].' '.$row['data_urodzenia'].' '.$row['dzial'].");
+	    echo("'.$row['id_pracownicy'].' '.$row['imie'].' '.$row['zarobki'].' '.$row['data_urodzenia'].' '.$row['dzial'].'");
  	    echo("</option>");
 	};
 echo('</select>)');
