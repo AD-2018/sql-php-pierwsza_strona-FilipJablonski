a125cf6697dba3bddc13bd87e7b4d18209f51130
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-12-11 18:31:50 +0100

    Create biblioteka_tab.php

---
+++ biblioteka_tab.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php
+require_once("connect.php");
+
+$sql = "INSERT INTO biblAutor_biblTytul (id,biblAutor,biblTytul,biblWypoz) 
+       VALUES (null,".'"'.$_POST['ida'].'"'.'"'.$_POST['idt'].'"'."0".')';
+
+
+if ($conn->query($sql) === TRUE) {
+  header('Location: https://jablonski-filip.herokuapp.com/biblioteka.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+</body>
+</html>
