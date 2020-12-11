d8ecf1a941a5bc5eabe860aa0a3bf3b8f6ba5234
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-12-11 19:10:24 +0100

    Update biblioteka_tab.php

--- biblioteka_tab.php
+++ biblioteka_tab.php
@@ -6,7 +6,7 @@
 <?php
 require_once("connect.php");
 
-$sql = "INSERT INTO biblAutor_biblTytul (id,biblAutor_id,biblTytul_id,biblWypoz) 
+$sql = "INSERT INTO biblAutor_biblTytul (id,biblAutor_id,biblTytul,biblWypoz) 
        VALUES (null,".'"'.$_POST['ida'].'"'.','.'"'.$_POST['idt'].'"'.','.'0'.')';
 
 
