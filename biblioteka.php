f26694c80018f437a570b4d3912fa2f311c40c39
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-12-11 19:24:37 +0100

    Update biblioteka.php

--- biblioteka.php
+++ biblioteka.php
@@ -146,7 +146,7 @@ $sql = "select (`biblAutor_biblTytul`.id) as ID_TAB, autor, tytul, biblWypoz fro
     echo("<th>ID</th><th>Autor</th><th>Tytuł</th><th>Wypożyczona</th>");
         while($row = mysqli_fetch_assoc($result)) {
             echo("<tr>");
-            echo("<td>".$row['ID_TAB']."</td><td>".$row['autor']."</td><td>".$row['tytul']."</td><td>");$row['biblWypoz']."</td>");
+            echo("<td>".$row['ID_TAB']."</td><td>".$row['autor']."</td><td>".$row['tytul']."</td><td>");$row['biblWypoz']."</td><td>");
 	    if ($row['biblWypoz']==0){
 		    echo("NIE</td>");
 	    };
