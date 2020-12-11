b15b7ebf7424e9e99fb7922c484a3c48c02b8a8c
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-12-11 18:02:49 +0100

    Update biblioteka.php

--- biblioteka.php
+++ biblioteka.php
@@ -135,7 +135,7 @@ echo ('<div><input type="submit" value="Wypożycz Książkę"></div>');
 echo ('</form>');
 echo ('</div>');
 echo ('<div class="Dane5">');
-$sql = "select (`biblAutor_biblTytul`.id) as ID_TAB, autor, tytul, biblWypoz from biblAutor_biblTytul,biblAutor,biblTytul where biblAutor.id=biblAutor_id and biblTytul.id=biblTytul_id order by autor,id asc";
+$sql = "select (`biblAutor_biblTytul`.id) as ID_TAB, autor, tytul, biblWypoz from biblAutor_biblTytul,biblAutor,biblTytul where biblAutor.id=biblAutor_id and biblTytul.id=biblTytul_id order by autor,ID_TAB asc";
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
       }
