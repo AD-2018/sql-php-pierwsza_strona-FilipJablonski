e7b4488bc0c298d9040dae5c75aa657a56dd4d8d
FilipJablonski <63042725+FilipJablonski@users.noreply.github.com>
2020-12-18 00:02:07 +0100

    Update testowa_gra.php

--- testowa_gra.php
+++ testowa_gra.php
@@ -96,8 +96,7 @@ $sql = "SELECT * FROM gracze";
      }
      while($row = mysqli_fetch_assoc($result)) {
 	     echo("<h2>Gracz ".$row[id].": ".$row[gracz]."<h1>");
-	     echo('</br>');
-	     echo ('<form action="biblioteka_ksiazka.php" method="POST">');
+	     echo ('<form action="nick'.$row[id].'php" method="POST">');
 	     echo ('<div><input type="text" name="nick"></div>');
 	     echo ('<div><input type="submit" value="Zmień Nick"></div>');
 	     echo ('</form>');
