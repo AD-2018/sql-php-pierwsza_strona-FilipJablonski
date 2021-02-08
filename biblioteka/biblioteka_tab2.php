675663a6b16c9627b7badf6c02dd586df3b0342c
FilipJablonski <jablonski.filip00@gmail.com>
2021-02-08 09:20:25 +0100

    mała poprawka

--- biblioteka/biblioteka_tab2.php
+++ biblioteka/biblioteka_tab2.php
@@ -6,7 +6,7 @@
 <?php
 require_once("../connect.php");
 
-$sql = "UPDATE biblAutor_biblTytul INNER JOIN biblAutor ON biblAutor.id=biblAutor_biblTytul.biblAutor_id SET biblWypoz= if (biblWypoz='0' , '1','0') WHERE autor=".'"'.$_POST['autor'].'"'.' and biblTytul_id='.'"'.$_POST['tytul'].'"';
+$sql = "UPDATE biblAutor_biblTytul INNER JOIN biblAutor ON biblAutor.id=biblAutor_biblTytul.biblAutor_id SET biblWypoz= if (biblWypoz='0' , '1','0') WHERE autor=".'"'.$_POST['autor'].'"'.' and biblTytul_id='.$_POST['tytul'];
 
 
 if ($conn->query($sql) === TRUE) {
