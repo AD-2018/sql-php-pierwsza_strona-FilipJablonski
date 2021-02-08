5d4135e8a52c5f9d0127c19132fdd2c1823f66bf
FilipJablonski <jablonski.filip00@gmail.com>
2021-02-08 09:25:57 +0100

    sprawdzenie post'a na stronie

--- biblioteka/biblioteka_tab2.php
+++ biblioteka/biblioteka_tab2.php
@@ -8,14 +8,16 @@ require_once("../connect.php");
 
 $sql = "UPDATE biblAutor_biblTytul INNER JOIN biblAutor ON biblAutor.id=biblAutor_biblTytul.biblAutor_id SET biblWypoz= if (biblWypoz='0' , '1','0') WHERE autor=".'"'.$_POST['autor'].'"'.' and biblTytul_id='.$_POST['tytul'];
 
-
 if ($conn->query($sql) === TRUE) {
-  header('Location: https://jablonski-filip.herokuapp.com/biblioteka/biblioteka.php');
+//  header('Location: https://jablonski-filip.herokuapp.com/biblioteka/biblioteka.php');
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
+
+echo ($sql);
+
 ?>
 </body>
 </html>
