7a1171f5770ce0c7573d67e2a33490e3fdc3f601
FilipJablonski <jablonski.filip00@gmail.com>
2021-04-14 13:26:51 +0200

    grid tabele

--- grid/index.php
+++ grid/index.php
@@ -12,15 +12,15 @@
 <div class="zawartosc">
     <div class="gridStrony">
         <strong>
-        <li class="gridLinkLi"><a class="gridLink" href="strona1.html">Strona 1</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="strona2.html">Strona 2</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="strona3.html">Strona 3</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="strona4.html">Strona 4</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="strona5.html">Strona 5</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="strona6.html">Strona 6</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="strona7.html">Strona 7</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="strona8.html">Strona 8</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="strona9.html">Strona 9</a></li>
+        <li class="gridLinkLi"><a class="gridLink" href="strona1.php">Strona 1</a></li>
+        <li class="gridLinkLi"><a class="gridLink" href="strona2.php">Strona 2</a></li>
+        <li class="gridLinkLi"><a class="gridLink" href="strona3.php">Strona 3</a></li>
+        <li class="gridLinkLi"><a class="gridLink" href="strona4.php">Strona 4</a></li>
+        <li class="gridLinkLi"><a class="gridLink" href="strona5.php">Strona 5</a></li>
+        <li class="gridLinkLi"><a class="gridLink" href="strona6.php">Strona 6</a></li>
+        <li class="gridLinkLi"><a class="gridLink" href="strona7.php">Strona 7</a></li>
+        <li class="gridLinkLi"><a class="gridLink" href="strona8.php">Strona 8</a></li>
+        <li class="gridLinkLi"><a class="gridLink" href="strona9.php">Strona 9</a></li>
         <li class="gridLinkLi"><a class="gridLink" href="strona10.html">Strona 10</a></li>
         <li class="gridLinkLi"><a class="gridLink" href="strona11.html">Strona 11</a></li>
         <li class="gridLinkLi"><a class="gridLink" href="strona12.html">Strona 12</a></li>
---
+++ grid/strona1.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Filip Jabłoński 3Ti</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="Grid.css">
+</head>
+
+<body>
+    <div class="str1">
+        <div class="str1A">
+            <?php
+                require_once("../connect.php");
+                $sql = "SELECT * FROM Osoby";
+                
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                      }
+                
+                    $result = mysqli_query($conn, $sql);
+                    if ( $result) {
+                         echo "<br>";
+                     } else {
+                       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                     }
+                
+                    echo("<h1>Tabelka</h1>");
+                
+                    echo("<table border='1'>");
+                    echo("<th>ID</th><th>Imię i Nazwisko</th>");
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo("<tr>");
+                            echo("<td>".$row['ID']."</td><td>".$row['Imie-Nazwisko']."</td>");
+                            echo("</tr>");
+                        };
+                    echo("</table>");
+                    echo ("<br>");
+            ?>
+        </div>
+        <div class="str1B">2</div>
+        <div class="str1C">3</div>
+        <div class="str1D">4</div>
+        <div class="str1E">5</div>
+    </div>
+</body>
+
+</html>
