5d45478518e264593692ed2923d425bf4d72d56e
Sebastian Swoboda <73635276+SebastianSwoboda216@users.noreply.github.com>
2021-04-14 14:07:53 +0200

    Update strona4.php

--- grid/strona4.php
+++ grid/strona4.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Grid 4</title>
-<meta charset="UTF-8">
-<link rel="stylesheet" href="Grid.css">
+	<title>Grid 4</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="Grid.css">
 </head>
 <body>
-<div class="str4">
-<div class="str4A">1</div>
-<div class="str4B">2</div>
-<div class="str4C">3</div>
-<div class="str4D">4</div>
-<div class="str4E">5</div>
-</div>
+	<div class="str4">
+		<div class="str4A">1</div>
+		<div class="str4B">2</div>
+		<div class="str4C">3</div>
+		<div class="str4D">4</div>
+		<div class="str4E">5</div>
+	</div>
 </body>
+
+</html>
 </html>
