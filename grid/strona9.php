c448dbb9785cdf1a32fc94eeb8b275c4e59185ef
FilipJablonski <jablonski.filip00@gmail.com>
2021-04-14 16:04:46 +0200

    grid

--- grid/strona9.php
+++ grid/strona9.php
@@ -39,8 +39,61 @@
                     echo ("<br>");
             ?>
         </div>
-        <div class="str9B">2</div>
-        <div class="str9C">3</div>
+        <div class="str9B">
+        <?php
+                require_once("../connect.php");
+                $sql = "SELECT * FROM projekt";
+                
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                      }
+                
+                    $result = mysqli_query($conn, $sql);
+                    if ( $result) {
+                         echo "<br>";
+                     } else {
+                       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                     }
+                
+                    echo("<h1>Projekty</h1>");
+                
+                    echo("<table border='1'>");
+                    echo("<th>ID</th><th>Projekt</th>");
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo("<tr>");
+                            echo("<td>".$row['ID']."</td><td>".$row['projekt']."</td>");
+                            echo("</tr>");
+                        };
+                    echo("</table>");
+                    echo ("<br>");
+            ?>
+        </div>
+        <div class="str9C">
+        <?php
+                        require_once("../connect.php");
+                        $sql = "select projekt, `Imie-Nazwisko`, (`WDW`.ID) as ID_TAB from `jablonski-filip_pbd`.WDW, `jablonski-filip_pbd`.projekt, `jablonski-filip_pbd`.Osoby where Osoby.ID=osoby_id and projekt.id=klasa_id order by ID_TAB asc";
+                            if ($conn->connect_error) {
+                                    die("Connection failed: " . $conn->connect_error);
+                             }
+                                $result = mysqli_query($conn, $sql);
+                            if ( $result) {
+                            } else {
+                                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                            }
+                        
+                            echo("<h1>Wiele do Wielu</h1>");
+                        
+                            echo("<table border='1'>");
+                            echo("<th>ID</th><th>Projekt</th><th>Pracownik</th>");
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo("<tr>");
+                                    echo("<td>".$row['ID_TAB']."</td><td>".$row['projekt']."</td><td>".$row['Imie-Nazwisko']."</td>");
+                                    echo("</tr>");
+                                };
+                            echo("</table>");
+                            echo ("<br>");
+                ?>
+        </div>
         <div class="str9D">4</div>
         <div class="str9E">Firma</div>
     </div>
