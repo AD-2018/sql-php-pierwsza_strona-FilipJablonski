479d16c956083ae2aa9807be8061aac0af3ab7e1
FilipJablonski <jablonski.filip00@gmail.com>
2021-04-14 13:39:41 +0200

    tabela 1

--- grid/strona1.php
+++ grid/strona1.php
@@ -10,36 +10,90 @@
 <body>
     <div class="str1">
         <div class="str1A">
-            <?php
-                require_once("../connect.php");
-                $sql = "SELECT * FROM Osoby";
-                
-                    if ($conn->connect_error) {
-                        die("Connection failed: " . $conn->connect_error);
-                      }
-                
-                    $result = mysqli_query($conn, $sql);
-                    if ( $result) {
-                         echo "<br>";
-                     } else {
-                       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-                     }
-                
-                    echo("<h1>Tabelka</h1>");
-                
-                    echo("<table border='1'>");
-                    echo("<th>ID</th><th>Imię i Nazwisko</th>");
-                        while($row = mysqli_fetch_assoc($result)) {
-                            echo("<tr>");
-                            echo("<td>".$row['ID']."</td><td>".$row['Imie-Nazwisko']."</td>");
-                            echo("</tr>");
-                        };
-                    echo("</table>");
-                    echo ("<br>");
+                <?php
+                        require_once("../connect.php");
+                        $sql = "SELECT * FROM biblAutor";
+                        
+                            if ($conn->connect_error) {
+                                die("Connection failed: " . $conn->connect_error);
+                              }
+                        
+                            $result = mysqli_query($conn, $sql);
+                            if ( $result) {
+                                 echo "<br>";
+                             } else {
+                               echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                             }
+                        
+                            echo("<h1>Autor</h1>");
+                        
+                            echo("<table border='1'>");
+                            echo("<th>ID</th><th>Autor</th>");
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo("<tr>");
+                                    echo("<td>".$row['id']."</td><td>".$row['autor']."</td>");
+                                    echo("</tr>");
+                                };
+                            echo("</table>");
+                            echo ("<br>");
+                ?>
             ?>
         </div>
-        <div class="str1B">2</div>
-        <div class="str1C">3</div>
+        <div class="str1B">                
+                <?php
+                        require_once("../connect.php");
+                        $sql = "SELECT * FROM biblTytul";
+                        
+                            if ($conn->connect_error) {
+                                die("Connection failed: " . $conn->connect_error);
+                              }
+                        
+                            $result = mysqli_query($conn, $sql);
+                            if ( $result) {
+                                 echo "<br>";
+                             } else {
+                               echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                             }
+                        
+                            echo("<h1>Tytuł</h1>");
+                        
+                            echo("<table border='1'>");
+                            echo("<th>ID</th><th>Tytuł</th>");
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo("<tr>");
+                                    echo("<td>".$row['id']."</td><td>".$row['tytul']."</td>");
+                                    echo("</tr>");
+                                };
+                            echo("</table>");
+                            echo ("<br>");
+                ?>
+            </div>
+        <div class="str1C">
+        <?php
+                        require_once("../connect.php");
+                        $sql = "select (`biblAutor_biblTytul`.id) as ID_TAB, autor, tytul, biblWypoz from biblAutor_biblTytul,biblAutor,biblTytul where biblAutor.id=biblAutor_id and biblTytul.id=biblTytul_id order by autor,ID_TAB asc";
+                            if ($conn->connect_error) {
+                                    die("Connection failed: " . $conn->connect_error);
+                             }
+                                $result = mysqli_query($conn, $sql);
+                            if ( $result) {
+                            } else {
+                                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                            }
+                        
+                            echo("<h1>Wiele do Wielu</h1>");
+                        
+                            echo("<table border='1'>");
+                            echo("<th>ID</th><th>Tytuł</th><th>Autor</th>");
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo("<tr>");
+                                    echo("<td>".$row['ID_TAB']."</td><td>".$row['tytul']."</td><td>".$row['autor']."</td>");
+                                    echo("</tr>");
+                                };
+                            echo("</table>");
+                            echo ("<br>");
+                ?>
+        </div>
         <div class="str1D">4</div>
         <div class="str1E">5</div>
     </div>
--- grid/strona2.php
+++ grid/strona2.php
@@ -9,7 +9,35 @@
 
 <body>
     <div class="str2">
-        <div class="str2A">1</div>
+        <div class="str2A">
+            <?php
+                require_once("../connect.php");
+                $sql = "SELECT * FROM Osoby";
+                
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                      }
+                
+                    $result = mysqli_query($conn, $sql);
+                    if ( $result) {
+                         echo "<br>";
+                     } else {
+                       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                     }
+                
+                    echo("<h1>Tabelka</h1>");
+                
+                    echo("<table border='1'>");
+                    echo("<th>ID</th><th>Imię i Nazwisko</th>");
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo("<tr>");
+                            echo("<td>".$row['ID']."</td><td>".$row['Imie-Nazwisko']."</td>");
+                            echo("</tr>");
+                        };
+                    echo("</table>");
+                    echo ("<br>");
+            ?>
+            </div>
         <div class="str2B">2</div>
         <div class="str2C">3</div>
         <div class="str2D">4</div>
