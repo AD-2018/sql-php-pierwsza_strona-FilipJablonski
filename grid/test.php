97e43918fd62fce6fd3b88c14c522b151dea8051
FilipJablonski <jablonski.filip00@gmail.com>
2021-04-19 09:59:50 +0200

    test

--- grid/test.php
+++ grid/test.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 require_once("../connect.php");
-$liczenie = "SELECT count(osoby_id) FROM `jablonski-filip_pbd`.WDW where osoby_id=4;"
+$liczenie = "SELECT count(osoby_id) FROM `jablonski-filip_pbd`.WDW where osoby_id=4";
 if (query($liczenie)>1){
     echo ("więcej");
 } else {
